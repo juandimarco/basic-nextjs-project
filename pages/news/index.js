af821e4961363ddd01f0122bdb53a745b8f7d6dc
juandimarco <68037364+juandimarco@users.noreply.github.com>
2022-06-29 18:24:23 -0300

    Link between pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,20 @@
+import { Fragment } from 'react';
+import Link from 'next/link';
+
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href='/news/nextjs-is-a-grat-frametwork'>
+            Nextjs Is A Great Frametwork
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default NewsPage;
